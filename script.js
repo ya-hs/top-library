4cb342112d8f78ccf5b5236db0e67b42b3a23990
ya-hs <68033139+ya-hs@users.noreply.github.com>
2025-06-24 16:15:44 +0100

    🚧 Script bones

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const btnAdd = document.querySelector("#btn-add");
-const book = document.querySelectorAll(".book");
-let bookText = "";
-book.textContent = bookText;
+const bookArea = document.querySelector("#book-area");
 
 btnAdd.addEventListener('click', () => {
-    addTextToBook();
+    console.log("click!");
+    addBook();
+
 })
 
-function addTextToBook() {
-    bookText = "test";
-    book.textContent = bookText;
+function addBook() {
+    const newBook = document.createElement("div");
+    const newTitle = document.createElement("h2");
+    const newAuthor = document.createElement("h3");
+    const newPages = document.createElement("h4");
+
+    newBook.classList.add("book-container");
+    newTitle.classList.add("book-title");
+    newAuthor.classList.add("book-author");
+    newPages.classList.add("book-pages");
+
+    newTitle.textContent = "Title";
+    newAuthor.textContent = "Author";
+    newPages.textContent = "100 Pages";
+    bookArea.appendChild(newBook);
+    newBook.appendChild(newTitle);
+    newBook.appendChild(newAuthor);
+    newBook.appendChild(newPages);
 }
