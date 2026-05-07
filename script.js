9500f0a291d75c1ff79b535fe551e0ce7b0b5144
ya-hs <68033139+ya-hs@users.noreply.github.com>
2026-05-07 09:57:34 +0100

    Rename variables and add classes to buttons

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const btnAdd = document.querySelector("#btn-add");
+const addButton = document.querySelector("#btn-add");
 const bookArea = document.querySelector("#book-area");
 const dialog = document.querySelector("#dialog");
-const btnSubmit = document.querySelector("#btn-submit");
-const btnCancel = document.querySelector("#btn-cancel");
-const fTitle = document.querySelector("#book-title");
-const fAuthor = document.querySelector("#book-author");
-const fPages = document.querySelector("#book-pages");
+const submitButton = document.querySelector("#btn-submit");
+const cancelButton = document.querySelector("#btn-cancel");
+const titleInput = document.querySelector("#book-title");
+const authorInput = document.querySelector("#book-author");
+const pagesInput = document.querySelector("#book-pages");
 const bookForm = document.querySelector("#add-book-form");
 const myLibrary = [];
 
-btnAdd.addEventListener('click', () => {
+addButton.addEventListener('click', () => {
     console.log("click!");
     dialog.showModal();
 })
 
-btnCancel.addEventListener('click', () => {
+cancelButton.addEventListener('click', () => {
     console.log("cancel!");
     dialog.close();
 })
 
-btnSubmit.addEventListener('click', () => {
+submitButton.addEventListener('click', () => {
     event.preventDefault();
     console.log("submit!");
     const fStatus = document.querySelector('input[name="book-status"]:checked');
-    addBookToLibrary(fTitle.value, fAuthor.value, fPages.value, fStatus.value);
+    addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, fStatus.value);
     displayBooks();
     dialog.close();
     bookForm.reset();
@@ -31,7 +31,7 @@ btnSubmit.addEventListener('click', () => {
 )
 
 function Book(title, author, pages, status) {
-    this.bookID = crypto.randomUUID();
+    this.bookId = crypto.randomUUID();
     this.bookTitle = title;
     this.bookAuthor = author;
     this.bookPages = pages;
@@ -52,10 +52,11 @@ function addBookToLibrary(title, author, pages, status) {
 function createRemoveButton(book) {
     const btnRemove = document.createElement("button")
     btnRemove.innerHTML = 'Remove';
-    btnRemove.dataset.id = book.bookID;
+    btnRemove.dataset.id = book.bookId;
+    btnRemove.className = "btn-remove";
     btnRemove.addEventListener('click', () => {
         const id = btnRemove.dataset.id;
-        const index = myLibrary.findIndex((book) => book.bookID === id);
+        const index = myLibrary.findIndex((book) => book.bookId === id);
         myLibrary.splice(index, 1);
         displayBooks();
     })
@@ -65,10 +66,11 @@ function createRemoveButton(book) {
 function createCycleButton(book) {
     const btnCycle = document.createElement("button");
     btnCycle.innerHTML = 'Toggle Status';
-    btnCycle.dataset.id = book.bookID;
+    btnCycle.dataset.id = book.bookId;
+    btnCycle.className = "btn-cycle";
     btnCycle.addEventListener('click', () => {
         const id = btnCycle.dataset.id;
-        const index = myLibrary.findIndex((book) => book.bookID === id);
+        const index = myLibrary.findIndex((book) => book.bookId === id);
         myLibrary[index].toggleStatus();
         displayBooks();
     })
